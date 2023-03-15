32c0b8e427c8de67b4caaffb89543ac9d830f4a2
nhnishat <nhnishat1998@gmail.com>
2023-03-16 00:36:13 +0600

    submit btn

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ const displayQuiz = (data) => {
 };
 
 // EventListener for quiz submit button
-document.querySelector('#submit').addEventlistener('click', () => {
+document.querySelector('#submit').addEventListener('click', () => {
 	console.log('click');
 	if (answers.length < 6) {
 		return;
